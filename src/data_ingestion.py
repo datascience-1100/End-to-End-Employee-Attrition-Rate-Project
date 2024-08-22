ea354b0812ca550c28e3bd0ed9b55ec1143e1d5d
Rina <datasciencejourney100@gmail.com>
2024-08-22 00:13:01 -0400

    data_ingestion file is improved- main function created to call individual functions

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -4,23 +4,30 @@ import os
 from sklearn.model_selection import train_test_split
 import yaml
 
-test_size=yaml.safe_load(open('params.yaml', 'r'))['data_ingestion']['test_size']
+def load_params(params_path):
+   test_size=yaml.safe_load(open(params_path, 'r'))['data_ingestion']['test_size']
+   return test_size
 
 # Path to the input CSV file
-input_file_path = "c:/Users/Subha/OneDrive/Desktop/HR_capstone_dataset.csv"
-
-# Read the CSV file into a DataFrame
-df = pd.read_csv(input_file_path)
-
-# Split the data into training and testing sets
-train_data, test_data = train_test_split(df, test_size=test_size, random_state=42)
+def read_data(url):
+   df = pd.read_csv(url)
+   return df
 
 # Define the path for saving the processed data
-data_path = os.path.join("data", "raw")
-
-# Create the directory if it does not exist
-os.makedirs(data_path, exist_ok=True)
-
-# Save the training and testing data to CSV files
-train_data.to_csv(os.path.join(data_path, 'train.csv'), index=False)
-test_data.to_csv(os.path.join(data_path, 'test.csv'), index=False)
+def save_data(data_path, train_data,test_data):
+    # Create the directory if it does not exist
+    os.makedirs(data_path, exist_ok=True)
+    # Save the training and testing data to CSV files
+    train_data.to_csv(os.path.join(data_path, 'train.csv'), index=False)
+    test_data.to_csv(os.path.join(data_path, 'test.csv'), index=False)
+    
+def main():
+    test_size=load_params('params.yaml')
+    df=read_data("c:/Users/Subha/OneDrive/Desktop/HR_capstone_dataset.csv")
+    # Split the data into training and testing sets
+    train_data, test_data = train_test_split(df, test_size=test_size, random_state=42)
+    data_path = os.path.join("data", "raw")
+    save_data(data_path, train_data,test_data)
+    
+if __name__=="__main__":
+    main()
